d50c02ec3d986905ad46027851c447bb491c4040
jaffarabbas <gamaportal8@gmail.com>
2023-08-06 13:23:03 +0500

    Commited File : IntegerFunctions.cs at 06/08/2023 1:23:03 pm

---
+++ DSA/DSA/Other/IntegerFunctions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Other
+{
+    internal class IntegerFunctions
+    {
+        public static int getFirstInt(int num)
+        {
+            while(num > 10)
+            {
+                num /= 10;
+            }
+            return num;
+        }
+        public static int counterIntegerWithOutString(int num)
+        {
+            int len = 0;
+            while(num > 0)
+            {
+                num /= 10;
+                len++;
+            }
+            return len;
+        }
+    }
+}
