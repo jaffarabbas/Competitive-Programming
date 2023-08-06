f8b8d28cfb426e7392ac1018e3118ac9a3fdf626
jaffarabbas <gamaportal8@gmail.com>
2023-08-06 13:23:09 +0500

    Commited File : Program.cs at 06/08/2023 1:23:09 pm

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -11,7 +11,7 @@ namespace DSA // Note: actual namespace depends on the project name.
         
         public static void Main()
         {
-            BROKENSTRING.func();
+            Console.WriteLine(IntegerFunctions.getFirstInt(465));
         }
 
     }
