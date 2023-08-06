cc8175cdd9ea63ece8f507efd19c0bdec028d45a
jaffarabbas <gamaportal8@gmail.com>
2023-08-06 13:23:03 +0500

    Commited File : IntegerCountWithOutString.cs at 06/08/2023 1:23:03 pm

---
+++ DSA/DSA/Other/IntegerCountWithOutString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Other
+{
+    internal class IntegerCountWithOutString
+    {
+        public static int counterIntegerWithOutString(int num)
+        {
+            int len = 0;
+            while(num > 0)
+            {
+                num /= 10;
+                len++;
+            }
+            return len;
+        }
+    }
+}
