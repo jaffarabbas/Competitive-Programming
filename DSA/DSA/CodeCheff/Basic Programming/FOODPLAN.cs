aa138663b3f82e78a215c1a25337af1bd39bb5e8
jaffarabbas <gamaportal8@gmail.com>
2023-08-11 22:37:19 +0500

    11-8-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/FOODPLAN.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class FOODPLAN
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                string[] lines = Console.ReadLine().Split(' ');
+                int a = int.Parse(lines[0]);
+                int b = int.Parse(lines[1]);
+                double temp = (double)a;
+                double f = a * 0.10;
+                temp = temp - f;
+                if (temp == b)
+                {
+                    Console.WriteLine("EITHER");
+                }
+                else if (temp < b)
+                {
+                    Console.WriteLine("ONLINE");
+                }
+                else
+                {
+                    Console.WriteLine("DINING");
+                }
+                t--;
+            }
+        }
+    }
+}
