50f52a0c4d0c5ea44affa7a11412c63b48010acc
jaffarabbas <gamaportal8@gmail.com>
2023-08-12 22:23:04 +0500

    Commited File : Program.cs at 12/08/2023 10:23:04 pm

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,18 +1,34 @@
-﻿using DSA.CodeCheff;
-using DSA.CodeCheff.Basic_Programming;
-using DSA.CodeCheff.Strings;
-using DSA.Other;
-using System;
+﻿using System;
 
 namespace DSA // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        
         public static void Main()
         {
-            FOODPLAN.func();
-        }
-
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                string v = "";
+                string[] arr = new string[8];
+                for (int i = 0; i < 8; i++)
+                {
+                    arr[i] = Console.ReadLine();
+                }
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        string temp = arr[i][j].ToString();
+                        if (!temp.Equals("."))
+                        {
+                            v += arr[i][j];
+                        }
+                    }
+                }
+                Console.WriteLine(v);
+                t--;
+            }
+        }   
     }
 }
