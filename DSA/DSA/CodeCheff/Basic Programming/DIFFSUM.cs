6afc306a309fad44a3a8fd6707ddea47d9e71f78
jaffarabbas <gamaportal8@gmail.com>
2023-08-12 21:20:58 +0500

    Create DIFFSUM.cs

---
+++ DSA/DSA/CodeCheff/Basic Programming/DIFFSUM.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class DIFFSUM
+    {
+        public static void func()
+        {
+            string[] lines = Console.ReadLine().Split(' ');
+            int a = int.Parse(lines[0]);
+            int b = int.Parse(lines[1]);
+            if(a > b)
+            {
+                int f = a - b;
+                Console.WriteLine(f);
+            }
+            else
+            {
+                int f = a + b;
+                Console.WriteLine(f);
+            }
+        }
+    }
+}
