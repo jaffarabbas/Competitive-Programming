5411f67d4c003d97140fff13e9a60dbbc92f13ea
jaffarabbas <gamaportal8@gmail.com>
2023-08-12 22:23:01 +0500

    Commited File : C.cs at 12/08/2023 10:23:01 pm

---
+++ DSA/DSA/CodeForces/1850/C.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeForces._1850
+{
+    internal class C
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                string v = "";
+                string[] arr = new string[8];
+                for (int i = 0; i < 8; i++)
+                {
+                    arr[i] = Console.ReadLine();
+                }
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 8; j++)
+                    {
+                        string temp = arr[i][j].ToString();
+                        if (!temp.Equals("."))
+                        {
+                            v += arr[i][j];
+                        }
+                    }
+                }
+                Console.WriteLine(v);
+                t--;
+            }
+        }
+    }
+}
