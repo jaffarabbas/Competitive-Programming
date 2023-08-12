5cc7d3b17df02ec6c3459f32bc46188a0cdd7725
jaffarabbas <gamaportal8@gmail.com>
2023-08-12 22:57:05 +0500

    Commited File : A.cs at 12/08/2023 10:57:05 pm

---
+++ DSA/DSA/CodeForces/1850/A.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeForces._1850
+{
+    internal class A
+    {
+        public static void func()
+        {
+            int a = int.Parse(Console.ReadLine());
+            while (a > 0)
+            {
+                string[] lines = Console.ReadLine().Split(' ');
+                int x = int.Parse(lines[0]);
+                int y = int.Parse(lines[1]);
+                int z = int.Parse(lines[2]);
+                if((x+y) > z || (x+z) > y)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+                a--;
+            }
+        }
+    }
+}
