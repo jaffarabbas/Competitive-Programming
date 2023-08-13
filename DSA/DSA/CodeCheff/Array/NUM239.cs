0fb376a4a9a1e1cd291c6c24d32a9f3f85116553
jaffarabbas <gamaportal8@gmail.com>
2023-08-13 22:31:55 +0500

    Commited File : NUM239.cs at 13/08/2023 10:31:55 pm

--- DSA/DSA/CodeCheff/Array/NUM239.cs
+++ DSA/DSA/CodeCheff/Array/NUM239.cs
@@ -8,5 +8,33 @@ namespace DSA.CodeCheff.Array
 {
     internal class NUM239
     {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                int count1 = 0;
+                string[] lines = Console.ReadLine().Split(' ');
+                int a = int.Parse(lines[0]);
+                int b = int.Parse(lines[1]);
+                for (int i = a;i<=b;i++)
+                {
+                    if(i > 10)
+                    {
+                        int temp = i % 10;
+                        if(temp == 2 || temp == 3 || temp == 9)
+                        {
+                            count1++;
+                        }
+                    }
+                    if(i == 2 || i == 3 || i == 9)
+                    {
+                        count1++;
+                    }
+                }
+                Console.WriteLine(count1);
+                t--;
+            }
+        }
     }
 }
