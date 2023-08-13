aaed28b144d2f50a9a00b6f2b5935e5419a25d6b
jaffarabbas <gamaportal8@gmail.com>
2023-08-13 22:31:49 +0500

    Commited File : B.cs at 13/08/2023 10:31:49 pm

---
+++ DSA/DSA/CodeForces/1760/B.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeForces._1760
+{
+    internal class B
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                int count = 0;
+                int n = int.Parse(Console.ReadLine());
+                char a = char.Parse(Console.ReadLine());
+                char[] arr = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+                for (int i = 0;  i < arr.Length; i++)
+                {
+                    if(a == arr[i])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+                t--;
+            }
+        }
+    }
+}
