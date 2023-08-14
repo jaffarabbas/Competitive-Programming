30ead012bdb1b15650cb618f5dfa13daa382aeff
jaffarabbas <gamaportal8@gmail.com>
2023-08-14 19:07:44 +0500

    Commited File : A.cs at 14/08/2023 7:07:43 pm

---
+++ DSA/DSA/CodeForces/1794/A.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeForces._1794
+{
+    internal class A
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                string[] lines = Console.ReadLine().Split(' ');
+                int fl = lines.Length / 2;
+                string[] par = new string[fl];
+                string[] por = new string[lines.Length - fl];
+                Array.Copy(lines, 0, par, 0, fl);
+                Array.Copy(lines, fl, por, 0, lines.Length - fl);
+                if (par.Equals(por))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
