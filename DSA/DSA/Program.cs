9a2f7713cdea3fe8e1f8db9c755c0f6e81d30c20
jaffarabbas <gamaportal8@gmail.com>
2023-08-14 11:27:32 +0500

    Commited File : Program.cs at 14/08/2023 11:27:32 am

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+using DSA.CodeCheff.Arrays;
+using System;
 
 namespace DSA // Note: actual namespace depends on the project name.
 {
@@ -6,25 +8,7 @@ namespace DSA // Note: actual namespace depends on the project name.
     {
         public static void Main()
         {
-            int t = int.Parse(Console.ReadLine());
-            while (t > 0)
-            {
-                int count = 0;
-                int n = int.Parse(Console.ReadLine());
-                string a = Console.ReadLine();
-                char[] arr = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
-                string f = String.Concat(a.OrderBy(c => c));
-                for (int i = 0; i < arr.Length; i++)
-                {
-                    count++;
-                    if (f.Last() == arr[i])
-                    {
-                        break;
-                    }
-                }
-                Console.WriteLine(count);
-                t--;
-            }
+            PAIREQ.func();
         }   
     }
 }
