54901a4f757ec774d2a62b0752531a1ce2290455
jaffarabbas <gamaportal8@gmail.com>
2023-08-14 19:07:49 +0500

    Commited File : Program.cs at 14/08/2023 7:07:49 pm

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,5 +1,5 @@
 ﻿
-using DSA.CodeCheff.Arrays;
+
 using System;
 
 namespace DSA // Note: actual namespace depends on the project name.
@@ -8,7 +8,30 @@ namespace DSA // Note: actual namespace depends on the project name.
     {
         public static void Main()
         {
-            PAIREQ.func();
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                string[] lines = Console.ReadLine().Split(' ');
+                int[] arr = Array.ConvertAll(lines, int.Parse);
+                int[] sorter = Array.ConvertAll(lines, int.Parse);
+                Array.Sort(sorter);
+                for (int i = 0; i < l; i++)
+                {
+                    if (arr[i] != sorter[sorter.Length - 1])
+                    {
+                        arr[i] -= sorter[sorter.Length - 1];
+                    }
+                    else
+                    {
+                        arr[i] -= sorter[sorter.Length - 2];
+                    }
+                }
+                foreach (var item in arr)
+                {
+                    Console.Write(item + " ");
+                }
+            }
         }   
     }
 }
