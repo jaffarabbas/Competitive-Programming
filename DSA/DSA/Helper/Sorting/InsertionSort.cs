1bbcae9e77cc3cf7a9bd4a2782a0f83e6eaa465f
jaffarabbas <gamaportal8@gmail.com>
2023-08-14 11:27:23 +0500

    Commited File : InsertionSort.cs at 14/08/2023 11:27:23 am

---
+++ DSA/DSA/Helper/Sorting/InsertionSort.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Helper.Sorting
+{
+    internal class InsertionSort
+    {
+        public static void IS(int[] inputArray)
+        {
+            for (int i = 0; i < inputArray.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (inputArray[j - 1] > inputArray[j])
+                    {
+                        int temp = inputArray[j - 1];
+                        inputArray[j - 1] = inputArray[j];
+                        inputArray[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
