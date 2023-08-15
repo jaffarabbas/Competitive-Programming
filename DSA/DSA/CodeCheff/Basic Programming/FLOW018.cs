01a5ac34c942c8fdd1e922f1bc52e8ad97bbe853
jaffarabbas <gamaportal8@gmail.com>
2023-08-15 21:11:00 +0500

    15-08-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/FLOW018.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class FLOW018
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while((t--) > 0)
+            {
+                int a = int.Parse(Console.ReadLine());
+                int v = 1;
+                for (int i = 1; i <= a; i++)
+                {
+                    v *= i;
+                }
+                Console.WriteLine(v);
+            }
+        }
+    }
+}
