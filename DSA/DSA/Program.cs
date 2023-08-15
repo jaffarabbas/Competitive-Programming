44a95ba46bfc4012ef9bc410a0ab5adfc225bfb3
jaffarabbas <gamaportal8@gmail.com>
2023-08-15 21:09:41 +0500

    Commited File : Program.cs at 15/08/2023 9:09:41 pm

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using DSA.CodeCheff.Basic_Programming;
 using System;
 
 namespace DSA // Note: actual namespace depends on the project name.
@@ -8,30 +9,7 @@ namespace DSA // Note: actual namespace depends on the project name.
     {
         public static void Main()
         {
-            int t = int.Parse(Console.ReadLine());
-            while ((t--) > 0)
-            {
-                int l = int.Parse(Console.ReadLine());
-                string[] lines = Console.ReadLine().Split(' ');
-                int[] arr = Array.ConvertAll(lines, int.Parse);
-                int[] sorter = Array.ConvertAll(lines, int.Parse);
-                Array.Sort(sorter);
-                for (int i = 0; i < l; i++)
-                {
-                    if (arr[i] != sorter[sorter.Length - 1])
-                    {
-                        arr[i] -= sorter[sorter.Length - 1];
-                    }
-                    else
-                    {
-                        arr[i] -= sorter[sorter.Length - 2];
-                    }
-                }
-                foreach (var item in arr)
-                {
-                    Console.Write(item + " ");
-                }
-            }
+            FLOW018.func();
         }   
     }
 }
