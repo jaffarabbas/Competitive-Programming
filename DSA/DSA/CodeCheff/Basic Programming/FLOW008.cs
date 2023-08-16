4ce4fdfcfff53887bb4c749707321dc9d06d5626
jaffarabbas <gamaportal8@gmail.com>
2023-08-16 20:52:43 +0500

    16-08-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/FLOW008.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class FLOW008
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while((t--) > 0)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if(a < 10 || a < 0)
+                {
+                    Console.WriteLine("Thanks for helping Chef!");
+                }
+                else
+                {
+                    Console.WriteLine(-1);
+                }
+            }
+        }
+    }
+}
--- DSA/DSA/CodeCheff/Basic Programming/JGAMES.cs
+++ DSA/DSA/CodeCheff/Basic Programming/JGAMES.cs
@@ -8,5 +8,37 @@ namespace DSA.CodeCheff.Basic_Programming
 {
     internal class JGAMES
     {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t > 0)
+            {
+                string[] lines = Console.ReadLine().Split(' ');
+                int a = int.Parse(lines[0]);
+                int b = int.Parse(lines[1]);
+                for (int i = 1; i <= b; i++)
+                {
+                    if (a > b)
+                    {
+                        //-
+                        a -= 1;
+                    }
+                    else
+                    {
+                        //+
+                        a += 1;
+                    }
+                }
+                if(a%2 == 0)
+                {
+                    Console.WriteLine("Janmansh");
+                }
+                else
+                {
+                    Console.WriteLine("Jay");
+                }
+                t--;
+            }
+        }
     }
 }
