822af27a0607a4c5c92665e6aa61dcb543f79af8
jaffarabbas <gamaportal8@gmail.com>
2023-08-17 21:58:43 +0500

    Create ECOCLASS.cs

---
+++ DSA/DSA/CodeCheff/Basic Programming/ECOCLASS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class ECOCLASS
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while((t--) > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                string[] arr1 = Console.ReadLine().Split(' ');
+                string[] arr2 = Console.ReadLine().Split(' ');
+
+                int[] ar1 = Array.ConvertAll(arr1, int.Parse);
+                int[] ar2 = Array.ConvertAll(arr2, int.Parse);
+                int count = 0;
+                for (int i = 0; i < l; i++) 
+                {
+                    if (ar1[i] == ar2[i])
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
