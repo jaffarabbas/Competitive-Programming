15b094ec465db412a66150599f40bbef82dfebfe
jaffarabbas <gamaportal8@gmail.com>
2023-08-18 22:10:31 +0500

    18-8-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/RCBPLAY.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class RCBPLAY
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                string[] lines = Console.ReadLine().Split(' ');
+                int a = int.Parse(lines[0]);
+                int b = int.Parse(lines[1]);
+                int c = int.Parse(lines[2]);
+
+                int f = a + (c * 2);
+                if (f >= b)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
