e51b74cff4c57aeb990014a6aea5bb5d99f56920
jaffarabbas <gamaportal8@gmail.com>
2023-08-22 21:41:22 +0500

    22-08-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CREDITS.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CREDITS
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if (a > 65)
+                {
+                    Console.WriteLine("Overload");
+                }
+                else if (a < 35)
+                {
+                    Console.WriteLine("Underload");
+                }
+                else
+                {
+                    Console.WriteLine("Normal");
+                }
+            }
+        }
+    }
+}
---
+++ DSA/DSA/CodeCheff/Basic Programming/HARDBET.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class HARDBET
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                string[] lines = Console.ReadLine().Split(" ");
+                int a = int.Parse(lines[0]);
+                int b = int.Parse(lines[1]);
+                int c = int.Parse(lines[2]);
+                if (a < b && a < c)
+                {
+                    Console.WriteLine("Draw");
+                }
+                else if (b < a && b < c)
+                {
+                    Console.WriteLine("Bob");
+                }
+                else if (c < a && c < b)
+                {
+                    Console.WriteLine("Alice");
+                }
+            }   
+        }
+    }
+}
