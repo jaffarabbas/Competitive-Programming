a2417015ab0da4cd8e285c4123be1d2b6336d880
jaffarabbas <gamaportal8@gmail.com>
2023-08-23 21:25:05 +0500

    Create SUPCHEF.cs

---
+++ DSA/DSA/CodeCheff/Basic Programming/SUPCHEF.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SUPCHEF
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while((t--) > 0)
+            {
+                string[] lines = Console.ReadLine().Split(" ");
+                int a = int.Parse(lines[0]);
+                int b = int.Parse(lines[1]);
+                int c = int.Parse(lines[2]);
+                int f= c * b;
+                if(f < a)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
