691659e949f104a2a21bd34d92547e0d6ff1fa7c
jaffarabbas <gamaportal8@gmail.com>
2023-08-25 22:49:55 +0500

    25-08-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/GENE01.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class GENE01
+    {
+        public static void func()
+        {
+            string[] lines = Console.ReadLine().Split(" ");
+            char a = char.Parse(lines[0]);
+            char b = char.Parse(lines[1]);
+            char[] l = { 'R', 'B', 'G' };
+
+        }
+    }
+}
---
+++ DSA/DSA/CodeCheff/Basic Programming/GOODWEAT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class GOODWEAT
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0) 
+            {
+                int count = 0, count2 = 0;
+                string[] l = Console.ReadLine().Split(' ');
+                int[] a = Array.ConvertAll(l, int.Parse);
+                foreach (int i in a)
+                {
+                    if(i == 1)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count2++;
+                    }
+                }
+                if(count > count2)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
