485558ca0dce7c2ab91753fc0f2c6f12231d9918
jaffarabbas <gamaportal8@gmail.com>
2023-08-28 21:24:58 +0500

    Create FLOW010.cs

---
+++ DSA/DSA/CodeCheff/Basic Programming/FLOW010.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class FLOW010
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                char a = Char.Parse(Console.ReadLine());
+                if(a == 'B' || a == 'b')
+                {
+                    Console.WriteLine("BattleShip");
+                }else if(a == 'C' || a == 'c')
+                {
+                    Console.WriteLine("Cruiser");
+                }else if(a == 'D' || a == 'd')
+                {
+                    Console.WriteLine("Destroyer");
+                }else if(a == 'F' || a == 'f')
+                {
+                    Console.WriteLine("Frigate");
+                }
+            }
+        }
+    }
+}
