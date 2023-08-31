79b5171186bf11f5e508139ac63e0f0bddf1772e
jaffarabbas <gamaportal8@gmail.com>
2023-08-31 21:29:47 +0500

    Create DEVSPORTS.cs

---
+++ DSA/DSA/CodeCheff/Basic Programming/DEVSPORTS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class DEVSPORTS
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                string[] lines = Console.ReadLine().Split(" ");
+                int x = int.Parse(lines[0]);
+                int y = int.Parse(lines[1]);
+                int a = int.Parse(lines[2]);
+                int b = int.Parse(lines[3]);
+                int c = int.Parse(lines[4]);
+                
+                if ((x - y) >= (a+b+c))
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
