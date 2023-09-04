02c4fdb09b7c44a2b0bbc92384777aa17649f56f
jaffarabbas <gamaportal8@gmail.com>
2023-09-04 21:34:21 +0500

    4-08-2023

--- DSA/DSA/CodeCheff/Basic Programming/CARCHOICE.cs
+++ DSA/DSA/CodeCheff/Basic Programming/CARCHOICE.cs
@@ -8,5 +8,29 @@ namespace DSA.CodeCheff.Basic_Programming
 {
     internal class CARCHOICE
     {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                string[] lines = Console.ReadLine().Split(" ");
+                double x = double.Parse(lines[0]);
+                double y = double.Parse(lines[1]);
+                double a = double.Parse(lines[2]);
+                double b = double.Parse(lines[3]);
+
+                if ((a / x) < (b / y))
+                {
+                    Console.WriteLine(-1);
+                }else if ((a / x) == (b / y))
+                {
+                    Console.WriteLine(0);
+                }
+                else
+                {
+                    Console.WriteLine(1);
+                }
+            }
+        }
     }
 }
--- DSA/DSA/CodeCheff/Basic Programming/ODDSEVENS.cs
+++ DSA/DSA/CodeCheff/Basic Programming/ODDSEVENS.cs
@@ -8,5 +8,23 @@ namespace DSA.CodeCheff.Basic_Programming
 {
     internal class ODDSEVENS
     {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                string[] lines = Console.ReadLine().Split(" ");
+                double x = double.Parse(lines[0]);
+                double y = double.Parse(lines[1]);
+                if((x + y) %2 == 0)
+                {
+                    Console.WriteLine("Bob");
+                }
+                else
+                {
+                    Console.WriteLine("Alice");
+                }
+            }
+        }
     }
 }
