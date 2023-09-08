4daf36bcd04cb52826ec654e4b6d1e32073ca3b3
jaffarabbas <gamaportal8@gmail.com>
2023-09-08 22:40:30 +0500

    08-09-2023

--- DSA/DSA/CodeCheff/Basic Programming/CARRYGOLD.cs
+++ DSA/DSA/CodeCheff/Basic Programming/CARRYGOLD.cs
@@ -8,5 +8,26 @@ namespace DSA.CodeCheff.Basic_Programming
 {
     internal class CARRYGOLD
     {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                string[] lines = Console.ReadLine().Split(" ");
+                int x = int.Parse(lines[0]);
+                int y = int.Parse(lines[1]);
+                int z = int.Parse(lines[2]);
+                x = x + 1;
+                int r = x * z;
+                if(r >= y)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
     }
 }
---
+++ DSA/DSA/CodeCheff/Basic Programming/FACEDIR.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class FACEDIR
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                int d = int.Parse(Console.ReadLine());
+                int a = 0;
+                for (int i = 1; i <= d; i++)
+                {
+                    a += 90;
+                    if (a > 270)
+                    {
+                        a = 0;
+                    }
+                }
+                if (a == 0)
+                {
+                    Console.WriteLine("North");
+                }
+                else if (a == 90)
+                {
+                    Console.WriteLine("East");
+                }
+                else if (a == 180)
+                {
+                    Console.WriteLine("South");
+                }
+                else if (a == 270)
+                {
+                    Console.WriteLine("West");
+                }
+            }
+        }
+    }
+}
