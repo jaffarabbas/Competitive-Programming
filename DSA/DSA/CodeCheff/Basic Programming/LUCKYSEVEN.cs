ffb07289fcaab1992c6fad212341bc40bf41e563
jaffarabbas <gamaportal8@gmail.com>
2023-09-12 21:34:15 +0500

    Create LUCKYSEVEN.cs

---
+++ DSA/DSA/CodeCheff/Basic Programming/LUCKYSEVEN.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class LUCKYSEVEN
+    {
+        public static void func()
+        {
+            string t = Console.ReadLine();
+            Console.WriteLine(t[6]);
+        }
+    }
+}
