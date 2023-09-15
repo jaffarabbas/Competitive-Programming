d1a581d52dc5793979956ebc90e5d6257946ca05
jaffarabbas <gamaportal8@gmail.com>
2023-09-15 23:39:51 +0500

    Commited File : Program.cs at 15/09/2023 11:39:50 pm

--- DSA/DSA/Program.cs
+++ DSA/DSA/Program.cs
@@ -10,80 +10,18 @@ namespace DSA
             int t = int.Parse(Console.ReadLine());
             while ((t--) > 0)
             {
-                int c = int.Parse(Console.ReadLine());
-                string[] lines = Console.ReadLine().Split(" ");
-                string[] lines2 = Console.ReadLine().Split(" ");
-                int[] a1 = Array.ConvertAll(lines, int.Parse);
-                int[] a2 = Array.ConvertAll(lines2, int.Parse);
-                bool f = false, f2 = false;
-                int count = 0;
-                for (int i = 0; i < c; i++)
+                string[] s = Console.ReadLine().Split(" ");
+                int a = Convert.ToInt32(st[0]);
+                int b = Convert.ToInt32(st[1]);
+                int c = Convert.ToInt32(st[2]);
+                int d = Convert.ToInt32(st[3]);
+                int count = 0, j;
+                for (j = 1; j <= 10; j++)
                 {
-                    if (a1[i] != a2[i])
-                    {
-                        if (a1[i] * 2 >= a2[i])
-                        {
-                            f = true;
-                        }
-                        else
-                        {
-                            f2 = false;
-                        }
-                        if (a2[i] >= a1[i] * 2)
-                        {
-                            f2 = true;
-                        }
-                        else
-                        {
-                            f = false;
-                        }
-                    }
-                    else
-                    {
-                        f = true;
-                        f2 = true;
-                    }
-                    if (f == f2)
-                    {
-                        count++;
-                    }
+                    if ((a <= j && j <= b) || (c <= j && j <= d)) count++;
                 }
                 Console.WriteLine(count);
             }
-            //int t = int.Parse(Console.ReadLine());
-            //while ((t--) > 0)
-            //{
-            //    int c = int.Parse(Console.ReadLine());
-            //    string[] lines = Console.ReadLine().Split(" ");
-            //    string[] lines2 = Console.ReadLine().Split(" ");
-            //    int[] a1 = Array.ConvertAll(lines, int.Parse);
-            //    int[] a2 = Array.ConvertAll(lines2, int.Parse);
-            //    int count = 0;
-            //    for (int i = 0; i < c; i++)
-            //    {
-            //        int h1 = 0, h2 = 0;
-            //        if (a1[i] != a2[i])
-            //        {
-            //            if (a1[i] * 2 > a2[i])
-            //            {
-            //                h1++;
-            //            }
-            //            if (a2[i] > a1[i] * 2)
-            //            {
-            //                h2++;
-            //            }
-            //        }
-            //        else
-            //        {
-            //            h1++;h2++;
-            //        }
-            //        if(h1 == h2)
-            //        {
-            //            count++;
-            //        }
-            //    }
-            //    Console.WriteLine(count);
-            //}
         }   
     }
 }
