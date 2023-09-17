9bb5fac9b5e959e1d36c9847b7746ff4b82d6f48
jaffarabbas <gamaportal8@gmail.com>
2023-09-17 21:53:13 +0500

    Create SAVWATER.cs

---
+++ DSA/DSA/CodeCheff/Basic Programming/SAVWATER.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SAVWATER
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                string[] st = Console.ReadLine().Split(" ");
+                int a = Convert.ToInt32(st[0]);
+                int b = Convert.ToInt32(st[1]);
+                int c = Convert.ToInt32(st[2]);
+                int d = Convert.ToInt32(st[3]);
+                
+            }
+        }
+    }
+}
