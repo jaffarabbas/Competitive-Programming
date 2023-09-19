b8d0b264946b6e4da8b41e40d9b829c3ce3a08a7
jaffarabbas <gamaportal8@gmail.com>
2023-09-19 21:45:32 +0500

    19-09-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/SHOEFIT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SHOEFIT
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                string[] st = Console.ReadLine().Split(" ");
+                int a = Convert.ToInt32(st[0]);
+                int b = Convert.ToInt32(st[1]);
+                int c = Convert.ToInt32(st[2]);
+                if(a == 0 && b == 1 || b == 1 && a == 0 || a == 0 && c == 1 || a == 1 && c == 0 || b == 0 && c == 1 || b == 1 && c == 0)
+                {
+                    Console.WriteLine(1);
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
