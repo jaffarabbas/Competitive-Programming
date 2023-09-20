aad2331460b7c43e99a9108d4aa7c2727d3c22bb
jaffarabbas <gamaportal8@gmail.com>
2023-09-20 21:55:53 +0500

    20-09-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/PENALTY.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class PENALTY
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                string[] lines = Console.ReadLine().Split(" ");
+                int[] arr = Array.ConvertAll(lines, int.Parse);
+                int count = 0 , count2 = 0;
+                foreach (var item in arr)
+                {
+                    if(item %2 == 0 && item == 1)
+                    {
+                        count++;
+                    }else if(item % 2 != 0 && item == 1)
+                    {
+                        count2++;
+                    }
+                }
+                if(count > count2)
+                {
+                    Console.WriteLine(1);
+                }else if(count < count2)
+                {
+                    Console.WriteLine(2);
+                }else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
