85a61d4cdc073595893b790cd1d81ae360a601b9
jaffarabbas <gamaportal8@gmail.com>
2023-09-21 21:43:55 +0500

    21-09-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/OFFBY1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class OFFBY1
+    {
+        public static void func()
+        {
+            string[] lines = Console.ReadLine().Split(" ");
+            int a  = int.Parse(lines[0]);
+            int b = int.Parse(lines[1]);
+            int r = a + b;
+            r = int.Parse(r.ToString() + "1");
+            Console.WriteLine(r);
+        }
+    }
+}
