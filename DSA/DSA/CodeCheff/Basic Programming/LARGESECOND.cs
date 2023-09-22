403e89564bb511e0c4dd846b4e038eb4310e5c97
jaffarabbas <gamaportal8@gmail.com>
2023-09-22 21:37:27 +0500

    22-09-2023

--- DSA/DSA/CodeCheff/Basic Programming/LARGESECOND.cs
+++ DSA/DSA/CodeCheff/Basic Programming/LARGESECOND.cs
@@ -1,11 +1,14 @@
-﻿
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace DSA 
+namespace DSA.CodeCheff.Basic_Programming
 {
-    internal class Program
+    internal class LARGESECOND
     {
-        public static void Main()
+        public static void func()
         {
             int t = int.Parse(Console.ReadLine());
             while ((t--) > 0)
@@ -13,22 +16,22 @@ namespace DSA
                 int l = int.Parse(Console.ReadLine());
                 string[] lines = Console.ReadLine().Trim().Split(" ");
                 int[] arr = Array.ConvertAll(lines, int.Parse);
-                int max1=int.MinValue, max2= int.MinValue;
+                int max1 = int.MinValue, max2 = int.MinValue;
                 foreach (int i in arr)
                 {
-                    if(i > max1)
+                    if (i > max1)
                     {
                         max2 = max1;
-                        max1=i;
+                        max1 = i;
                     }
-                    if(i > max2 && i != max1)
+                    if (i > max2 && i != max1)
                     {
-                        max2=i;
+                        max2 = i;
                     }
                 }
 
-                Console.WriteLine(max1+max2);
+                Console.WriteLine(max1 + max2);
             }
-        }   
+        }
     }
 }
