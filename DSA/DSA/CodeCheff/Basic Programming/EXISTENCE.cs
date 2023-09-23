ccd6bed2a5c09a0b04b108d1724eda89cd43c42f
jaffarabbas <gamaportal8@gmail.com>
2023-09-23 20:49:30 +0500

    23-09-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/EXISTENCE.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class EXISTENCE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while((t--) > 0)
+            {
+                string[] lines = Console.ReadLine().Split(" ");
+                long a = long.Parse(lines[0]);
+                long b = long.Parse(lines[1]);
+                if(a*a*a*a+4*b*b == 4*a*a*b)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
