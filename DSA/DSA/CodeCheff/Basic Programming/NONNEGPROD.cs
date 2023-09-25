8e6543d227072dfe6cc24f9cb7592a5ab97caa2d
jaffarabbas <gamaportal8@gmail.com>
2023-09-25 23:36:50 +0500

    25-09-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/NONNEGPROD.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class NONNEGPROD
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int r = 1;
+                for (int i = 0; i < l; i++)
+                {
+                    if (arr[i] < 0)
+                    {
+                        r *= -1;
+                    }else if (arr[i] == 0)
+                    {
+                        r = 0;
+                        break;
+                    }
+                }
+                if (r < 0)
+                {
+                    Console.WriteLine(1);
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
