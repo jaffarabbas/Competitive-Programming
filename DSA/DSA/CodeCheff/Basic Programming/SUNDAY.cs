911526e88aef20374a8f692e66fe1807c6034386
jaffarabbas <gamaportal8@gmail.com>
2023-09-27 21:26:06 +0500

    27-09-2923

---
+++ DSA/DSA/CodeCheff/Basic Programming/SUNDAY.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SUNDAY
+    {
+        public static void func()
+        {
+            int T = int.Parse(Console.ReadLine());
+            for (int t = 0; t < T; t++)
+            {
+                int N = int.Parse(Console.ReadLine());
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+                int holiday = 8;
+                for (int j = 0; j < N; j++)
+                {
+                    if (arr[j] != 6 && arr[j] != 7 && arr[j] != 13 && arr[j] != 14 && arr[j] != 20 && arr[j] != 21 && arr[j] != 27 && arr[j] != 28)
+                    {
+                        holiday++;
+                    }
+                }
+                Console.WriteLine(holiday);
+            }
+        }
+    }
+}
--- DSA/DSA/CodeCheff/Basic Programming/THREEQ.cs
+++ DSA/DSA/CodeCheff/Basic Programming/THREEQ.cs
@@ -8,5 +8,34 @@ namespace DSA.CodeCheff.Basic_Programming
 {
     internal class THREEQ
     {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while((t--) > 0)
+            {
+                int[] arr1 = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int[] arr2 = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int count = 0,count2 = 0;
+                for (int i = 0; i < arr1.Length; i++)
+                {
+                    if (arr1[i] == 1)
+                    {
+                        count++;
+                    }
+                    if (arr2[i] == 1)
+                    {
+                        count2++;
+                    }
+                }
+                if(count > count)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
     }
 }
