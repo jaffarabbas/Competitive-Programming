62251aeb3ef0cc12fc5aef6d114a6ddad7ba23fc
jaffarabbas <gamaportal8@gmail.com>
2023-09-28 21:57:20 +0500

    28-09-02023

---
+++ DSA/DSA/CodeCheff/Basic Programming/NEWSPAPER.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class NEWSPAPER
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            Console.WriteLine(t > 7? "YES" : "NO");
+        }
+    }
+}
