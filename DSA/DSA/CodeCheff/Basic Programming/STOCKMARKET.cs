7f3ae65f6135e111f401f5b3e14cd066aa325585
jaffarabbas <gamaportal8@gmail.com>
2023-09-29 18:54:19 +0500

    29-09-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/STOCKMARKET.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class STOCKMARKET
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());  
+            while (t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                Array.Sort(arr);
+                int f = 0;
+                for (int i = 1; i < l; i++)
+                {
+                    f += arr[i];
+                }
+                Console.WriteLine(f);
+            }
+        }
+    }
+}
