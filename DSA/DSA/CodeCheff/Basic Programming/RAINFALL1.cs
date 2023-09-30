75275d5639939fdbcc1bb8fdba185bfbc4bdc97b
jaffarabbas <gamaportal8@gmail.com>
2023-09-30 21:49:28 +0500

    30-09-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/RAINFALL1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    public class RAINFALL1
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while(t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                if(l < 3)
+                {
+                    Console.WriteLine("LIGHT");
+                }else if(l >= 7)
+                {
+                    Console.WriteLine("HEAVY");
+                }
+                else if(l >= 3 && l < 7)
+                {
+                    Console.WriteLine("MODERATE");
+                }
+            }
+        }
+    }
+}
