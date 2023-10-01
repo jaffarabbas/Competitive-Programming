7614993c920df31367a77a7f1982c8c16a2cc24f
jaffarabbas <gamaportal8@gmail.com>
2023-10-01 18:54:33 +0500

    01-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/EQUALDIST.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class EQUALDIST
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if (l[0] == l[1])
+                {
+                    Console.WriteLine("YES");
+                }else if ((l[0] + l[1]) %2 == 0)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
