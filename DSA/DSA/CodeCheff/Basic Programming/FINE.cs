285b16c9b4856ed134b72e7f7388f810d3d9ce21
jaffarabbas <gamaportal8@gmail.com>
2023-10-03 23:30:06 +0500

    03-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/FINE.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class FINE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                if (l > 100)
+                {
+                    Console.WriteLine(2000);
+                }
+                else if (l <= 70)
+                {
+                    Console.WriteLine(0);
+                }
+                else if (l > 70 && l <= 100)
+                {
+                    Console.WriteLine(500);
+                }
+            }
+        }
+    }
+}
