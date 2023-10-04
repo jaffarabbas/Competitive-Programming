d2aee534a3886efa4f6752049dd85ca79950d46b
jaffarabbas <gamaportal8@gmail.com>
2023-10-04 21:37:26 +0500

    04-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CHESSTIME.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CHESSTIME
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while(t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                l *= 60;
+                int r = l / 20;
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
