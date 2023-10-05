169b2f86335cbf2b3e01b2610ab4082ba16dfbdb
jaffarabbas <gamaportal8@gmail.com>
2023-10-05 21:33:12 +0500

    05-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/MAXDIFFMIN.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class MAXDIFFMIN
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int max = l.Max();
+                int min = l.Min();
+                Console.WriteLine(max - min);
+            }
+        }
+    }
+}
