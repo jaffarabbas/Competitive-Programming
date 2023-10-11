f39eefdbc5cc210f6dce57e67f20f9633d7aaed8
jaffarabbas <gamaportal8@gmail.com>
2023-10-11 22:28:46 +0500

    11-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/SLEEP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SLEEP
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int a = int.Parse(Console.ReadLine());
+                if(a < 7)
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
