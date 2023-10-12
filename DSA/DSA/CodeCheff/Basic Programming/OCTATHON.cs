efe5cfc55ca97672739dd4c537cfd427e46c720b
jaffarabbas <gamaportal8@gmail.com>
2023-10-12 21:34:08 +0500

    12-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/OCTATHON.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class OCTATHON
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            if(t < 3)
+            {
+                Console.WriteLine("GOLD");
+            }else if(t >= 3 && t < 6)
+            {
+                Console.WriteLine("SILVER");
+            }else
+            {
+                Console.WriteLine("BRONZE");
+            }
+        }
+    }
+}
