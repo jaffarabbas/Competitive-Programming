c20edf7b22b81fe6ca09aaac66df3f5ee2b74fbb
jaffarabbas <gamaportal8@gmail.com>
2023-10-13 22:03:03 +0500

    11-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/M1ENROL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class M1ENROL
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int a = l[1] - l[0];
+                Console.WriteLine(a < 0 ? 0 : a);
+            }
+        }
+    }
+}
