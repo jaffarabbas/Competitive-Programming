da0be6b5b129961a61ec5f17f6e270ade7df6b16
jaffarabbas <gamaportal8@gmail.com>
2023-10-15 20:33:12 +0500

    15-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/PPSUM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class PPSUM
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int a = 0;
+                while (l[0] > 0)
+                {
+                    int s = 0;
+                    for (int i = 1; i <= l[1]; i++)
+                    {
+                        s += i;
+                    }
+                    l[1] = s;
+                    a = s;
+                    l[0]--;
+                }
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
