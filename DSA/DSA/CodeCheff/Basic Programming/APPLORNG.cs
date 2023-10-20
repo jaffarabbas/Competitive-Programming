70c25f46e7f2a7f22eb40908c4ad68576c21e731
jaffarabbas <gamaportal8@gmail.com>
2023-10-20 22:04:05 +0500

    20-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/APPLORNG.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class APPLORNG
+    {
+        public static void func()
+        {
+            int a = int.Parse(Console.ReadLine());
+            int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            Console.WriteLine(a >= l[0] + l[1] ? "YES" : "NO");
+        }
+    }
+}
