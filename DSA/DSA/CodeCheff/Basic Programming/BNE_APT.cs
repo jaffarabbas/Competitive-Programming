76ff9070a006e2fd9fd78d5d5e90fc2d5d23d167
jaffarabbas <gamaportal8@gmail.com>
2023-10-26 21:47:56 +0500

    26-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/BNE_APT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class BNE_APT
+    {
+        public static void func()
+        {
+            int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int[] l2 = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            Console.WriteLine(l[0] * l2[0] + l[1] * l2[1]);
+        }
+    }
+}
