45ae6ded55e833776ca64edd168366538c702d01
jaffarabbas <gamaportal8@gmail.com>
2023-10-27 22:31:54 +0500

    27-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/AIRHOCKEY.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class AIRHOCKEY
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int max = l[0] > l[1] ? l[0] : l[1];
+                Console.WriteLine(7 - max);
+            }
+        }
+    }
+}
