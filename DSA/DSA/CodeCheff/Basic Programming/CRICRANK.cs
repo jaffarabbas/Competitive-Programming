5df7bcbdc8f00267dca68bdef3d47591d6523365
jaffarabbas <gamaportal8@gmail.com>
2023-10-28 12:45:31 +0500

    28-10-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CRICRANK.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CRICRANK
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int[] l2 = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int count = 0, count2 = 0;
+                for (int i = 0; i < l.Length; i++)
+                {
+                    if (l[i] > l2[i])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count2++;
+                    }
+                }
+                if(count > count2)
+                {
+                    Console.WriteLine("A");
+                }
+                else
+                {
+                    Console.WriteLine("B");
+                }
+            }
+        }
+    }
+}
