e63f8dfb23f11255b4f5b932ff6d7cb819436c71
jaffarabbas <gamaportal8@gmail.com>
2023-11-05 11:27:26 +0500

    05-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CHEFBOTTLE.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CHEFBOTTLE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int a = 0;
+                int count = 0;
+                for (int i = 1; i <= l[0]; i++) {
+                    a = l[1] * i;
+                    if (a > l[2])
+                        break;
+                    count++;
+                }
+                Console.WriteLine(count);
+            }
+        }
+    }
+}
