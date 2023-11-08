22aec0d4fb2a4931c203520f7582162c5de0f56f
jaffarabbas <gamaportal8@gmail.com>
2023-11-08 21:46:28 +0500

    11-08-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/EMAILREM.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class EMAILREM
+    {
+        public static void func()
+        {
+            int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            Console.WriteLine(l[0] - l[1]);
+        }
+    }
+}
