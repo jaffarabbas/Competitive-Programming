27049c2240430cdd2454711f8a432385bbbdeed2
jaffarabbas <gamaportal8@gmail.com>
2023-11-10 22:24:27 +0500

    10-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CLEARDAY.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CLEARDAY
+    {
+        public static void func()
+        {
+            int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            Console.WriteLine(7 - l[0] - l[1]);
+        }
+    }
+}
---
+++ DSA/DSA/CodeCheff/Basic Programming/SIXFRIENDS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SIXFRIENDS
+    {
+        public static void func()
+        {
+            int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int a = l[0] * 3;
+            int b = l[1] * 2;
+            Console.WriteLine(a < b ? a : b);
+        }
+    }
+}
