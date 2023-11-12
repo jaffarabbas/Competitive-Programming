a906cfe7d36a7aff0a3715cdf6326d11f6bfe8e0
jaffarabbas <gamaportal8@gmail.com>
2023-11-12 23:04:32 +0500

    12-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/KITCHENSPICE.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class KITCHENSPICE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                 int l = int.Parse(Console.ReadLine());
+                 if( l < 4)
+                {
+                    Console.WriteLine("MILD");
+                }else if (l >= 4 && l < 7)
+                {
+                    Console.WriteLine("MEDIUM");
+                }
+                else
+                {
+                    Console.WriteLine("HOT");
+                }
+            }
+        }
+    }
+}
