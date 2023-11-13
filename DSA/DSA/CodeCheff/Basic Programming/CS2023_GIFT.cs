2386f33a450ece0212fb8259a8f908fcafe02b06
jaffarabbas <gamaportal8@gmail.com>
2023-11-13 21:14:48 +0500

    13-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CS2023_GIFT.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CS2023_GIFT
+    {
+        public static void func()
+        {
+            int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int a = l[1] - l[0];
+            Console.WriteLine(a <= l[2] ? "YES" : "NO");
+        }
+    }
+}
