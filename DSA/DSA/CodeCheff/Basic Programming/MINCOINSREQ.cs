f675f03a797d594029cac33132d8223bf01f157e
jaffarabbas <gamaportal8@gmail.com>
2023-11-14 22:09:13 +0500

    14-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/MINCOINSREQ.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class MINCOINSREQ
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                int a = l % 10;
+                Console.WriteLine(a);
+            }
+        }
+    }
+}
