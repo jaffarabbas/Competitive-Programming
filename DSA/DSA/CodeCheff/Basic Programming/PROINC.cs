da11bbada9889046b400dff7174465ce1f930531
jaffarabbas <gamaportal8@gmail.com>
2023-11-24 21:59:51 +0500

    24-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/PROINC.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class PROINC
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                double a = l[0] * 0.1;
+                Console.WriteLine((l[0] + a) - (l[0] - l[1]));
+            }
+        }
+    }
+}
