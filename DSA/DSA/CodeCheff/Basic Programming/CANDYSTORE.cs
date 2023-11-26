99db0e90a412af82b1d8e71c9858c665940f387f
jaffarabbas <gamaportal8@gmail.com>
2023-11-26 11:08:16 +0500

    26-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CANDYSTORE.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CANDYSTORE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if (l[1] <= l[0])
+                {
+                    Console.WriteLine(l[1]);
+                }
+                else
+                {
+                    int a = l[1] - l[0];
+                    Console.WriteLine(l[0] + a * 2);
+                }
+            }
+        }
+    }
+}
