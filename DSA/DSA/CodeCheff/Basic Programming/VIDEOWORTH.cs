3c27b4988ab677e49c242e9df5d65dd569a11665
jaffarabbas <gamaportal8@gmail.com>
2023-11-27 21:18:45 +0500

    27-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/VIDEOWORTH.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class VIDEOWORTH
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int t2 = int.Parse(Console.ReadLine());
+                Console.WriteLine((t2 * 24) * 1000);
+            }
+        }
+    }
+}
