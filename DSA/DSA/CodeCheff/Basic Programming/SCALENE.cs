7d8fce41af0523cb7e8f7039ebd870617a7003d6
jaffarabbas <gamaportal8@gmail.com>
2023-11-28 21:33:19 +0500

    28-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/SCALENE.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SCALENE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if (l[0] == l[1] || l[0] == l[2] || l[1] == l[2])
+                {
+                    Console.WriteLine("YES");
+                }else 
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
