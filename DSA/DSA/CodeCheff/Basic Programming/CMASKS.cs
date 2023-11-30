637e4bee06a3b27b4dd66a7a288836281c41dfef
jaffarabbas <gamaportal8@gmail.com>
2023-11-30 21:08:13 +0500

    30-11-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CMASKS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CMASKS
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if (l[0] * 100 >= l[1] * 10)
+                {
+                    Console.WriteLine("Cloth");
+                }else 
+                {
+                    Console.WriteLine("Disposable");
+                }
+            }
+        }
+    }
+}
