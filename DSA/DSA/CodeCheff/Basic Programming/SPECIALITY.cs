ae455a773332b94507a56bb2b4cd899da81d88d1
jaffarabbas <gamaportal8@gmail.com>
2023-12-01 23:26:33 +0500

    01-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/SPECIALITY.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SPECIALITY
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if (l[0] > l[1] && l[0] > l[2])
+                {
+                    Console.WriteLine("Setter");
+                }else if (l[1] > l[0] && l[1] > l[2])
+                {
+                    Console.WriteLine("Tester");
+                }
+                else if (l[2] > l[0] && l[2] > l[1])
+                {
+                    Console.WriteLine("Editorialist");
+                }
+            }
+        }
+    }
+}
