a0bc3a59b47c3a83917da21443e7d94cc2d22c57
jaffarabbas <gamaportal8@gmail.com>
2023-12-04 22:02:12 +0500

    04-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/TVDISC.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class TVDISC
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if (l[0] - l[2] < l[1] - l[3])
+                {
+                    Console.WriteLine("First");
+                }else if (l[0] - l[2] > l[1] - l[3])
+                {
+                    Console.WriteLine("Second");
+                }
+                else
+                {
+                    Console.WriteLine("Any");
+                }
+            }
+        }
+    }
+}
