9b13f7fe6ff1a46974d17a79a3624a40b9328420
jaffarabbas <gamaportal8@gmail.com>
2023-12-05 22:47:43 +0500

    Create BROKENPHONE.cs

---
+++ DSA/DSA/CodeCheff/Basic Programming/BROKENPHONE.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class BROKENPHONE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if (l[0] < l[1])
+                {
+                    Console.WriteLine("REPAIR");
+                }else if (l[0] > l[1])
+                {
+                    Console.WriteLine("NEW PHONE");
+                }
+                else
+                {
+                    Console.WriteLine("ANY");
+                }
+            }
+        }
+    }
+}
