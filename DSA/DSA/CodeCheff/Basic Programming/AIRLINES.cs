c07f20b10fb8d94b3a90a5b9a643392e1707b3c1
jaffarabbas <gamaportal8@gmail.com>
2023-12-16 22:58:00 +0500

    16-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/AIRLINES.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class AIRLINES
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int m = l[0] * 10;
+                Console.WriteLine(m > l[1] ? l[1] * l[2] : m * l[2]);
+            }
+        }
+    }
+}
