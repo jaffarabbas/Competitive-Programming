bd2b5bda3e6026051cdc0be12ec07c0f7011c1ff
jaffarabbas <gamaportal8@gmail.com>
2023-12-17 19:42:30 +0500

    17-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/EXAMCHEF.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class EXAMCHEF
+    {
+        public void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int a = l[0] * l[1];
+                float p = a * 0.5f;
+                Console.WriteLine(l[2] > p ? "YES" : "NO");
+            }
+        }
+    }
+}
