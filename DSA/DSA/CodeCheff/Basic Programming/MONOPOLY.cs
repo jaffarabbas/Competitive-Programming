af0f8a7af711f705eda64bcf858457e3931396e5
jaffarabbas <gamaportal8@gmail.com>
2023-12-18 23:04:12 +0500

    Create MONOPOLY.cs

---
+++ DSA/DSA/CodeCheff/Basic Programming/MONOPOLY.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class MONOPOLY
+    {
+        public void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                Array.Sort(l);
+                Console.WriteLine(l[0] + l[1] < l[2] ? "YES" : "NO");
+            }
+        }
+    }
+}
