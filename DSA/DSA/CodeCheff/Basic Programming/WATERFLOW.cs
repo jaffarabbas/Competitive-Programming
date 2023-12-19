b411af4d8d9275c2f2f50b2b2761eec345790cdc
jaffarabbas <gamaportal8@gmail.com>
2023-12-19 22:54:44 +0500

    19-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/WATERFLOW.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class WATERFLOW
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+                int n = l[0] + (l[2] * l[3]);
+                if (n < l[1])
+                {
+                    Console.WriteLine("Unfilled");
+                }
+                else if(n > l[1])
+                {
+                    Console.WriteLine("overFlow");
+                }
+                else if(n == l[1])
+                {
+                    Console.WriteLine("filled");
+                }
+            }
+        }
+    }
+}
