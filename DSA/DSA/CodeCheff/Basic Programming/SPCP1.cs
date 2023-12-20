5060c9aba091d51334f5e4d46e1beb2a045af8b6
jaffarabbas <gamaportal8@gmail.com>
2023-12-20 22:43:11 +0500

    20-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/SPCP1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SPCP1
+    {
+        public static void func()
+        {
+            int[] l = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+            Console.WriteLine(l[0] >= 60 && l[1] <= 130 ? "YES" : "NO");
+        }
+    }
+}
