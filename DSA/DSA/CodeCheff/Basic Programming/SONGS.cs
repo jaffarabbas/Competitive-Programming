7d5a0258deaa631d8f857022ce14096acf6c3ece
jaffarabbas <gamaportal8@gmail.com>
2023-12-23 21:56:24 +0500

    23-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/SONGS.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SONGS
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+                if (l[0] > l[1])
+                {
+                    Console.WriteLine(l[0] / (l[1] * 3));
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
