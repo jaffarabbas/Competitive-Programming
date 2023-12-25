63f4911d7d32292f2d1c187895741b5abbfdc4c6
jaffarabbas <gamaportal8@gmail.com>
2023-12-25 18:18:00 +0500

    25-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/MANGOES.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class MANGOES
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+                int temp = 0;
+                for (int i = 1; i <= l[2]; i++)
+                {
+                    int a = ((i * l[0]) + l[1]);
+                    if(a <= l[2])
+                    {
+                        temp = i;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                Console.WriteLine(temp);
+            }
+        }
+    }
+}
---
+++ DSA/DSA/CodeCheff/Basic Programming/TWOROOKS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class TWOROOKS
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+                Console.WriteLine(l[0] == l[2] || l[1] == l[3] ? "YES" : "NO");
+            }
+        }
+    }
+}
