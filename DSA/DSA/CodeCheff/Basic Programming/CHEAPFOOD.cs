64af937fc38fb9ad5cb629a232041eed91f524e9
jaffarabbas <gamaportal8@gmail.com>
2023-12-29 21:19:55 +0500

    29-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CHEAPFOOD.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CHEAPFOOD
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while ((t--) > 0)
+            {
+                int a = int.Parse(Console.ReadLine());
+                Console.WriteLine(a > 1000 ? a * 0.10 : 100);
+            }
+        }
+    }
+}
