5692657f53267a572f6a4dc43188d1a411ed6ea5
jaffarabbas <gamaportal8@gmail.com>
2023-12-30 19:24:47 +0500

    30-12-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/CPRIVAL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    public partial class CPRIVAL
+    {
+        public static void func()
+        {
+            int[] l = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+            int[] l2 = Array.ConvertAll(Console.ReadLine().Trim().Split(), int.Parse);
+            Console.WriteLine(l[0] + l2[0] > l[1] + l2[1] ? "Dominater" : "Everule");
+        }
+    }
+}
