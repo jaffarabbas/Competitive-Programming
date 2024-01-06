2b26514517320daa091ac9d460b83c1617d084d3
jaffarabbas <gamaportal8@gmail.com>
2024-01-06 22:29:18 +0500

    06-01-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/JASSIGNMENTS.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class JASSIGNMENTS
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                if(l < 10)
+                {
+                    if(10 - l >= 3)
+                    {
+                        Console.WriteLine("YES");
+                    }
+                    else
+                    {
+                        Console.WriteLine("NO");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
