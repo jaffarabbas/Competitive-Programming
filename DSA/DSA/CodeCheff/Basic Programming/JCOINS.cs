98ca56350ef63ea55e4d42a95cdf938ac38b77ad
jaffarabbas <gamaportal8@gmail.com>
2024-01-08 22:45:37 +0500

    8-1-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/JCOINS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class JCOINS
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                Console.WriteLine((l[0] * 10) + (l[1] * 5));
+            }
+        }
+    }
+}
