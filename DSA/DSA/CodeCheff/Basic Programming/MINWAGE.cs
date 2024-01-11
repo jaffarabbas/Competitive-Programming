228dfee54cf9948c7f94a36c04cfcbea1580550a
jaffarabbas <gamaportal8@gmail.com>
2024-01-11 21:22:48 +0500

    11-01-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/MINWAGE.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class MINWAGE
+    {
+        public static void func()
+        {
+            Console.WriteLine(int.Parse(Console.ReadLine()) > 11 ? "YES" : "NO");
+        }
+    }
+}
