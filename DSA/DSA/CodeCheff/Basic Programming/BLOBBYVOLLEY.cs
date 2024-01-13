3499e053e741d2bf1736a0345ac751b454633dd0
jaffarabbas <gamaportal8@gmail.com>
2024-01-13 13:57:43 +0500

    13-01-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/BLOBBYVOLLEY.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class BLOBBYVOLLEY
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                char[] list = Console.ReadLine().ToArray();
+                int a = 0, b = 0;
+                bool s = true;
+                for (int i = 0; i < l; i++)
+                {
+                    if (list[i] == 'A')
+                    {
+                        if (s)
+                        {
+                            a++;
+                        }
+                        s = true;
+                    }
+                    else
+                    {
+                        if (!s)
+                        {
+                            b++;
+                        }
+                        s = false;
+                    }
+                }
+                Console.WriteLine(a + " " + b);
+            }
+        }
+    }
+}
