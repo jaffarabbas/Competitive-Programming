a27ac5f64e7239f5aa9e7f01e39c94deb98e1f3d
jaffarabbas <gamaportal8@gmail.com>
2024-01-20 18:47:00 +0500

    20-01-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/NOTEBOOK.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class NOTEBOOK
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                Console.WriteLine((l * 1000)/100) ;
+            }
+        }
+    }
+}
