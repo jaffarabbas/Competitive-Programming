4ddba679a8f61912f67155e25f91b2b754fa28fd
jaffarabbas <gamaportal8@gmail.com>
2024-01-21 21:27:26 +0500

    Inserted DSA/CodeCheff/Basic Programming/SUGARCANE.cs at 21/01/2024 21:27:26

---
+++ DSA/DSA/CodeCheff/Basic Programming/SUGARCANE.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SUGARCANE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                int a = l * 50;
+                int a2 = (a * 20) / 100;
+                int a3 = (a * 20) / 100;
+                int a4 = (a * 30) / 100;
+                Console.WriteLine(a - ((a2 + a3) +a4));
+            }
+        }
+    }
+}
