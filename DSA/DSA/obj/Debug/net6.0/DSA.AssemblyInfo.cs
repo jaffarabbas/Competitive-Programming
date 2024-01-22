9392a9400929b7a18a76c3f883b7fcd0085c455c
jaffarabbas <gamaportal8@gmail.com>
2024-01-22 20:55:36 +0500

    Updated DSA/obj/Debug/net6.0/DSA.AssemblyInfo.cs at 22/01/2024 20:55:36

--- DSA/DSA/obj/Debug/net6.0/DSA.AssemblyInfo.cs
+++ DSA/DSA/obj/Debug/net6.0/DSA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DSA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a27ac5f64e7239f5aa9e7f01e39c94deb98e1f3d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DSA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DSA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
