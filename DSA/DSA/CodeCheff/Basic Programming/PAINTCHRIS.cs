62b72ea07c07d02199d283417867a017e7da638f
jaffarabbas <gamaportal8@gmail.com>
2024-01-23 22:51:33 +0500

    Inserted DSA/CodeCheff/Basic Programming/PAINTCHRIS.cs at 23/01/2024 22:51:33

---
+++ DSA/DSA/CodeCheff/Basic Programming/PAINTCHRIS.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class PAINTCHRIS
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int s2 = l[2] / 2;
+                int s3 = l[0] * l[1];
+                Console.WriteLine(s2 / s3);
+            }
+        }
+    }
+}
