af1ec4414dc3881413670bbcdd0164c5c228ad76
jaffarabbas <gamaportal8@gmail.com>
2024-01-24 22:22:18 +0500

    Inserted DSA/CodeCheff/Basic Programming/CHEFCAND.cs at 24/01/2024 22:22:17

---
+++ DSA/DSA/CodeCheff/Basic Programming/CHEFCAND.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class CHEFCAND
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if (l[0] >= l[1])
+                {
+                    if ((l[0] - l[1]) % 4 == 0)
+                    {
+                        Console.WriteLine((l[0] - l[1]) / 4);
+                    }
+                    else
+                    {
+                        Console.WriteLine((l[0] - l[1]) / 4 + 1);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
