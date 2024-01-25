889878ca0e4294b66cf23a75ab6c09b50c4b98d0
jaffarabbas <gamaportal8@gmail.com>
2024-01-25 21:27:17 +0500

    Inserted DSA/CodeCheff/Basic Programming/IED.cs at 25/01/2024 21:27:17

---
+++ DSA/DSA/CodeCheff/Basic Programming/IED.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class IED
+    {
+        public static void func()
+        {
+            int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+            int a = l[0] * l[2];
+            int b = l[1] * l[2];
+            Console.WriteLine(a > b ? a : b);
+        }
+    }
+}
