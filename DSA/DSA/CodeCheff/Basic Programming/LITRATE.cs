6c719ae8ab68fc355db003e958b7a74350c25737
jaffarabbas <gamaportal8@gmail.com>
2024-01-26 22:17:21 +0500

    Inserted DSA/CodeCheff/Basic Programming/LITRATE.cs at 26/01/2024 22:17:21

---
+++ DSA/DSA/CodeCheff/Basic Programming/LITRATE.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class LITRATE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int a = l[0] * (int)0.75;
+                Console.WriteLine((l[0] / l[1]) * 100 >= a ? "YES" : "NO");
+            }
+        }
+    }
+}
