19511164e2dc55fd226b41066d37f512ecac945a
jaffarabbas <gamaportal8@gmail.com>
2024-01-27 22:13:50 +0500

    Inserted DSA/CodeCheff/Basic Programming/ACCURACY.cs at 27/01/2024 22:13:50

---
+++ DSA/DSA/CodeCheff/Basic Programming/ACCURACY.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class ACCURACY
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                int c = l % 3;
+                Console.WriteLine(c == 0 ? 0 : (3 - c));
+            }
+        }
+    }
+}
