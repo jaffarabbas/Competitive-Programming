ae60263fc431bedd0fd3065bf62d27e31ac2f144
jaffarabbas <gamaportal8@gmail.com>
2024-01-28 21:17:02 +0500

    Inserted DSA/CodeCheff/Basic Programming/ACTEMP.cs at 28/01/2024 21:17:02

---
+++ DSA/DSA/CodeCheff/Basic Programming/ACTEMP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class ACTEMP
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if(l[0] <= l[1] && l[1] >= l[2])
+                {
+                    Console.WriteLine("YES");
+                }
+                else
+                {
+                    Console.WriteLine("NO");
+                }
+            }
+        }
+    }
+}
