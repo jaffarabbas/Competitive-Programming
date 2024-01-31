c5e5228c5a712ebce527bb5d3579ac2edeabc8a3
jaffarabbas <gamaportal8@gmail.com>
2024-01-31 21:54:40 +0500

    31-01-2023

---
+++ DSA/DSA/CodeCheff/Basic Programming/BIRDFARM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class BIRDFARM
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(""), int.Parse);
+                if (l[2] % l[0] == 0 && l[2] % l[1] == 0)
+                {
+                    Console.WriteLine("ANY");
+                }
+                else if (l[2] % l[0] == 0 && l[2] % l[1] != 0)
+                {
+                    Console.WriteLine("CHICKEN");
+                }
+                else if (l[2] % l[0] != 0 && l[2] % l[1] == 0)
+                {
+                    Console.WriteLine("DUCK");
+                }
+                else
+                {
+                    Console.WriteLine("NONE");
+                }
+            }
+        }
+    }
+}
---
+++ DSA/DSA/CodeCheff/Basic Programming/FLOW007.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class FLOW007
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(""), int.Parse);   
+                for (int i = l.Length - 1;i>=0; i--)    
+                {
+                    if (l[i] == 0)
+                    {
+                        continue;
+                    }
+                    Console.Write(l[i]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
