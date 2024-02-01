6e0cfa26e5f4c1bb73cede98d0beb38c1975d580
jaffarabbas <gamaportal8@gmail.com>
2024-02-01 22:36:45 +0500

    Inserted DSA/CodeCheff/Basic Programming/ADVITIYA1.cs at 01/02/2024 22:36:45

---
+++ DSA/DSA/CodeCheff/Basic Programming/ADVITIYA1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class ADVITIYA1
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            Console.WriteLine(t == 16 || t == 17 || t == 18 ? "ADVITIYA": "WAITING FOR ADVITIYA");
+        }
+    }
+}
