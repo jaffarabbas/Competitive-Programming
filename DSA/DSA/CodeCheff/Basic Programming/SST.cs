89373a709536606ecfda0f4070ef1c6e0c4db122
jaffarabbas <gamaportal8@gmail.com>
2024-02-06 21:25:02 +0500

    Inserted DSA/CodeCheff/Basic Programming/SST.cs at 06/02/2024 21:25:02

---
+++ DSA/DSA/CodeCheff/Basic Programming/SST.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class SST
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                
+            }
+        }
+    }
+}
