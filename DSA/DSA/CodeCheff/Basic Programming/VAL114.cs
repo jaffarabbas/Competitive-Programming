a64864084756f07865da27737f53b34173d3c5a0
jaffarabbas <gamaportal8@gmail.com>
2024-02-08 13:25:50 +0500

    Inserted DSA/CodeCheff/Basic Programming/VAL114.cs at 08/02/2024 13:25:50

---
+++ DSA/DSA/CodeCheff/Basic Programming/VAL114.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class VAL114
+    {
+        public static void func()
+        {
+            int l = int.Parse(Console.ReadLine());
+            Console.WriteLine(l == 121 ? "Likely" : "Unlikely");
+        }
+    }
+}
