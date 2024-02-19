f350a1fd23c83f5141294c33fde490dca3374b7d
jaffarabbas <gamaportal8@gmail.com>
2024-02-19 21:05:39 +0500

    Inserted DSA/CodeCheff/Basic Programming/MAXTASTE.cs at 19/02/2024 21:05:39

---
+++ DSA/DSA/CodeCheff/Basic Programming/MAXTASTE.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class MAXTASTE
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int a = l[0] > l[1] ? l[0] : l[1];
+                int b = l[2] > l[3] ? l[2] : l[3];
+                Console.WriteLine(a+b);
+            }
+        }
+    }
+}
