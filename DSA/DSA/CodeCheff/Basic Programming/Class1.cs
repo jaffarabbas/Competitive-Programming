416f855f773e6ff99bf3814028b7fba9c2168875
jaffarabbas <gamaportal8@gmail.com>
2024-02-25 23:04:08 +0500

    Inserted DSA/CodeCheff/Basic Programming/Class1.cs at 25/02/2024 23:04:08

---
+++ DSA/DSA/CodeCheff/Basic Programming/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class Class1
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int d = l[0] - l[1];
+                if (d > l[1])
+                {
+                    Console.WriteLine(l[1]);
+                }
+                else
+                {
+                    Console.WriteLine(d);
+                }
+            }
+        }
+    }
+}
