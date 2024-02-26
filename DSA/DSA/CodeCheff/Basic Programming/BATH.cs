18fcd2c59c79694461269669df337dd5d770c362
jaffarabbas <gamaportal8@gmail.com>
2024-02-26 21:25:29 +0500

    Inserted DSA/CodeCheff/Basic Programming/BATH.cs at 26/02/2024 21:25:28

---
+++ DSA/DSA/CodeCheff/Basic Programming/BATH.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class BATH
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] l = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                if ((l[1] * 2) <= l[0])
+                {
+                    Console.WriteLine(l[0] / (2 * l[1]));
+                }
+                else
+                {
+                    Console.WriteLine(0);
+                }
+            }
+        }
+    }
+}
