c663e65d090be2c213f6d4e2a216c38c23df7b41
jaffarabbas <gamaportal8@gmail.com>
2024-02-28 22:29:43 +0500

    Inserted DSA/CodeCheff/Basic Programming/PBREV.cs at 28/02/2024 22:29:43

---
+++ DSA/DSA/CodeCheff/Basic Programming/PBREV.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class PBREV
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int l = int.Parse(Console.ReadLine());
+                int[] li = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int count = 0;
+                for (int i = 0; i < l; i++)
+                {
+                    if (li[i] > 4)
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine(count == l ? "YES" : "NO");
+            }
+        }
+    }
+}
