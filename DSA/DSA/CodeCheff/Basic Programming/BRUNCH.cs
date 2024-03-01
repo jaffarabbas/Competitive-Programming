42ec2ecbcf12d751d012e26a8de149ce85ab51e2
jaffarabbas <gamaportal8@gmail.com>
2024-03-01 22:48:28 +0500

    Inserted DSA/CodeCheff/Basic Programming/BRUNCH.cs at 01/03/2024 22:48:28

---
+++ DSA/DSA/CodeCheff/Basic Programming/BRUNCH.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.CodeCheff.Basic_Programming
+{
+    internal class BRUNCH
+    {
+        public static void func()
+        {
+            int t = int.Parse(Console.ReadLine());
+            while (t-- > 0)
+            {
+                int[] li = Array.ConvertAll(Console.ReadLine().Split(" "), int.Parse);
+                int a = li[0] / li[1];
+                Console.WriteLine(a > 20 ? 20 : a);
+            }
+        }
+    }
+}
